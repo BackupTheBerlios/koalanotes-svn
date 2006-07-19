f240b6d9ff3accf6fbca6add1f4cf3707f5953e4
alison <alison@3f4a8262-cf0a-0410-a549-b64dce640af4>
2006-07-19 10:49:08 +0000

    Any Controller subclass that's worthy of existing will need to have a reference to the Dispatcher, for doing things like creating popup dialogs.

--- trunk/src/de/berlios/koalanotes/controllers/Controller.java
+++ trunk/src/de/berlios/koalanotes/controllers/Controller.java
@@ -12,7 +12,7 @@ public abstract class Controller {
 	private static final Class[] METHOD_ARGS = new Class[] {Event.class};
 	private static final String SEPARATOR = ".";
 	
-	private Dispatcher d;
+	protected Dispatcher d;
 	
 	public Controller(Dispatcher d) {
 		this.d = d;
